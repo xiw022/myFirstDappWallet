c8417398d4ba20360ad7142ee38eea6ec6077390
brucewxh <brucewxh@brucewxhdeair.lan>
2018-07-25 16:24:31 -0700

    second

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -2,7 +2,54 @@ contract SimpleWallet {
    address owner;
    mapping(address => bool) isAllowedToSendFundsMapping;
 
- function SimpleWallet() {
-   owner = msg.sender;
- }
+   event Deposit(address _sender, uint amount);
+   event Withdrawl(address _sender, uint amount, address _beneficiary);
+
+   function SimpleWallet() {
+     owner = msg.sender;
+   }
+
+   function() {
+     if(msg.sender == owner || isAllowedToSendFundsMapping[msg.sender] == true) {
+       Deposit(msg.sender, msg.value);
+     }
+     else {
+       throw;
+     }
+    }
+
+
+    function sendFunds(uint amount, address receiver) returns (uint) {
+      if(msg.sender == owner || isAllowedToSendFundsMapping[msg.sender] == true) {
+        if(this.balance >= amount) {
+          if(!receiver.send(amount)) {
+            throw;
+          }
+          Withdrawl(msg.sender, amount, receiver);
+          return this.balance;
+        }
+      }
+    }
+
+    function allowAddressToSendMoney(address _address) {
+      if(msg.sender == owner) {
+          isAllowedToSendFundsMapping[_address] = true;
+      }
+    }
+
+    function disAllowAddressToSendMoney(address _address) {
+      if(msg.sender == owner) {
+          isAllowedToSendFundsMapping[_address] = false;
+      }
+    }
+
+    function isAllowedToSend(address _address) constant returns (bool) {
+        return isAllowedToSendFundsMapping[_address] || _address== owner;
+    }
+
+    function killWallet() {
+        if(msg.sender == owner) {
+            suicide(owner);
+        }
+    }
 }
